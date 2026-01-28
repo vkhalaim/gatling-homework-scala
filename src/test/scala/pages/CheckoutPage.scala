52d46d6d5541fd34c85327aad6d6b8fc6da0a737
Vasyl Khalaim <vasyl.khalaim@ukr.net>
2026-01-28 22:46:43 +0200

    checkout page added

---
+++ src/test/scala/pages/CheckoutPage.scala
@@ -0,0 +1,62 @@
+package pages
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import base.BaseHelpers._
+import io.gatling.core.structure.ChainBuilder
+
+object CheckoutPage {
+
+  val open: ChainBuilder =
+    group("09_Open_Checkout_Page") {
+      exec(
+        http("Open Checkout")
+          .get("/checkout")
+          .check(status.is(200))
+          .check(regex("name=\"cart_content\" value='(.+?)'").saveAs("cartContent"))
+          .check(regex("name=\"total_net\" value=\"(.+?)\"").saveAs("totalNet"))
+          .check(regex("name=\"trans_id\" value=\"(\\d+)\"").saveAs("transId"))
+      )
+        .pause(minThinkTime, maxThinkTime)
+    }
+
+  val submit: ChainBuilder =
+    group("10_Submit_Order") {
+
+      exec { session =>
+        session
+          .set("city", "Kyiv")
+          .set("country", "UA")
+          .set("postalCode", "01001")
+          .set("redirectUrl", "/thank-you")
+          .set("comment", "")
+          .set("fullName", s"User_${session.userId}")
+          .set("email", s"user${session.userId}@test.com")
+          .set("phone", s"+380${500000000 + session.userId}")
+          .set("address", s"Street ${session.userId}")
+      }
+        .exec(
+          http("Place Order")
+            .post("/checkout")
+            .asMultipartForm
+            .formParam("cart_city", "#{city}")
+            .formParam("cart_content", "#{cartContent}")
+            .formParam("ic_formbuilder_redirect", "#{redirectUrl}")
+            .formParam("cart_submit", "Place Order")
+            .formParam("cart_comment", "#{comment}")
+            .formParam("trans_id", "#{transId}")
+            .formParam("cart_type", "order")
+            .formParam("cart_country", "#{country}")
+            .formParam("shipping", "order")
+            .formParam("cart_name", "#{fullName}")
+            .formParam("cart_phone", "#{phone}")
+            .formParam("cart_address", "#{address}")
+            .formParam("cart_postal", "#{postalCode}")
+            .formParam("cart_email", "#{email}")
+            .formParam("total_net", "#{totalNet}")
+            .check(status.is(200))
+        )
+        .pause(minThinkTime, maxThinkTime)
+    }
+
+}
