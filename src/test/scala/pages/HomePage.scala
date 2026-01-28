50aaee366675f59e3948d6dd4230c4e722764367
Vasyl Khalaim <vasyl.khalaim@ukr.net>
2026-01-28 22:21:48 +0200

    added home page

---
+++ src/test/scala/pages/HomePage.scala
@@ -0,0 +1,21 @@
+package pages
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import base.BaseHelpers._
+import io.gatling.core.structure.ChainBuilder
+
+object HomePage {
+
+  val open: ChainBuilder =
+    group("01_Open_Home_Page") {
+      exec(
+        http("Open Home")
+          .get("/")
+          .check(status.is(200))
+          .check(regex("Performance testing Essentials").exists)
+      )
+        .pause(minThinkTime, maxThinkTime)
+    }
+
+}
