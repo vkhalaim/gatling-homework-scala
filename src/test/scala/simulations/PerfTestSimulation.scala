4398a39c422d4bf6f7c4e7f8eff8839a8f917c4b
Vasyl Khalaim <vasyl.khalaim@ukr.net>
2026-01-28 22:53:29 +0200

    added simulation class and flow

---
+++ src/test/scala/simulations/PerfTestSimulation.scala
@@ -0,0 +1,70 @@
+package simulations
+
+import io.gatling.core.Predef._
+
+import scala.concurrent.duration._
+import base.BaseHelpers._
+import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
+import pages._
+
+class PerfTestSimulation extends Simulation {
+
+  /*
+   * Run command:
+   * mvn gatling:test \
+   *  -Dgatling.simulationClass=simulations.PerfTestSimulation \
+   *  -DbaseUrl=http://localhost
+   */
+
+  // -------------------------
+  // Scenario definition
+  // -------------------------
+  val PerfTestSimulation: ScenarioBuilder = scenario("User_Journey")
+    .exitBlockOnFail {
+      exec(flushState)
+
+        // 1. Open application
+        .exec(HomePage.open)
+
+        // 2. Navigate to Tables
+        .exec(TablesPage.open)
+        .exec(TablesPage.openRandomTable)
+        .exec(TablesPage.addToCart)
+
+        // 3. 50% users add Chair
+        .randomSwitch(
+          50.0 -> exec(ChairsPage.flow)
+        )
+
+        // 4. 30% of ALL users go to checkout
+        .randomSwitch(
+          30.0 -> exec(
+            CartPage.open
+              .exec(CheckoutPage.open)
+              .exec(CheckoutPage.submit)
+          )
+        )
+    }
+
+  // -------------------------
+  // Injection profiles
+  // -------------------------
+
+  // Open model (mandatory)
+  val openModel: PopulationBuilder = PerfTestSimulation.inject(
+    atOnceUsers(100)
+  )
+
+  // Closed model (advanced, optional)
+  val closedModel: PopulationBuilder = PerfTestSimulation.inject(
+    constantConcurrentUsers(100).during(30.seconds)
+  )
+
+  // -------------------------
+  // Setup
+  // -------------------------
+  setUp(
+    openModel
+    // closedModel   // uncomment if needed
+  ).protocols(httpProtocol)
+}
