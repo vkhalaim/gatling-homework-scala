6a6d7c94faac389579eac260c9b52fbcb759f912
Vasyl Khalaim <vasyl.khalaim@ukr.net>
2026-03-03 15:21:57 +0200

    added parametrization for test run

--- src/test/scala/simulations/PerfTestSimulation.scala
+++ src/test/scala/simulations/PerfTestSimulation.scala
@@ -7,15 +7,23 @@ import base.BaseHelpers._
 import scenarios.ShopScenario._
 
 class PerfTestSimulation extends Simulation {
+  val users: Int =
+    Integer.getInteger("users", 50)
+
+  val ramp: Int =
+    Integer.getInteger("ramp", 30)
+
+  val duration: Int =
+    Integer.getInteger("duration", 300)
 
   /*
    * Run command. PerfTestSimulation was added to pom.xml:
-   * mvn gatling:test
+   * mvn gatling:test -Dusers=80 -Dramp=150 -Dduration=1800
    */
 
   setUp(
     scn.inject(
-      atOnceUsers(100) // open model
+      rampUsers(users).during(ramp.seconds) // open model
       // constantConcurrentUsers(100).during(30.seconds) // closed model
     )
   ).protocols(httpProtocol)
